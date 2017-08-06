89e5caabcda83bfb6a1f262aa20f7c6ef3ce6f0b
Bc. Peter Horváth <info@opensencillo.com>
2017-08-06 21:00:37 +0200

    timelapse from jpg to avi

---
+++ examples/timelapse.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Timelapse - name output file:"
+read timelapseout
+echo "Timelapse - frames per seconds (e.g. 20):"
+read timelapsefps
+echo "Timelapse - remove name cache"
+rm *.txt
+echo "... done step 1 of 3"
+echo "Timelapse - create name cache"
+ls -t *.jpg
+ls -t *.jpg > cachenames.txt
+echo "... done step 2 of 3"
+echo "Timelapse - create timelapse mpeg4 movie ${timelapseout}.avi"
+mencoder -nosound -ovc lavc -lavcopts vcodec=mpeg4 -o "$timelapseout".avi -mf type=jpeg:fps="$timelapsefps" mf://@cachenames.txt
+echo "Timelapse - ${timelapseout}.avi ... done step 3 of 3"
+echo "Timelapse - end"
+exit 0
